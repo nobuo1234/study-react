b75bb4e452937e87ef836ce96799fc233b399172
nobe_deve <wfn.1209@gmail.com>
2022-02-27 02:14:08 +0900

    修正

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./Main.module.css";
+import { Headline } from "src/components/Headline";
+import { Links } from "src/components/Links";
+
+export function Main(props) {
+
+	return (
+		<main className={styles.main}>
+			<Headline page={props.page}>
+				<code className={styles.code}>pages/{props.page}.js</code>
+			</Headline>
+			<Links />
+		</main>
+	);
+}
