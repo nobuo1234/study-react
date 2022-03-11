8166c817fe5cc6a9dc790c68de5f610f878b3e34
nobe_deve <wfn.1209@gmail.com>
2022-03-11 22:00:14 +0900

    不要なコードの削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,35 +1,12 @@
 import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
-const About = (props) => {
+const About = () => {
 	return (
 		<div className={styles.container}>
 			<Header />
-			{props.isShow ? <h1>{props.doubleCount}</h1> : null}
-			<button type="button" onClick={props.handleClick}>
-				ボタン
-			</button>
-			<button type="button" onClick={props.handleDisplay}>
-				{props.isShow ? "非表示" : "表示"}
-			</button>
-
-			<input
-				type="text"
-				name=""
-				value={props.text}
-				onChange={props.handleChange}
-			/>
-			<button onClick={props.handleAdd}>追加</button>
-			<ul>
-				{props.array.map((item) => {
-					return <li key={item}>{item}</li>;
-				})}
-			</ul>
-
-			<Main page="about" />
-			<Footer />
+			<h1>Next.jsで学ぶReact講座</h1>
+			<p>JSONPlaceholderのAPIを叩くよ</p>
 		</div>
 	);
 };
